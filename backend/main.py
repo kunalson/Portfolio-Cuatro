6107feb59a46b18cac1d744cb98345cac24aa4e4
KunalSonz <kunalsonawane009@gmail.com>
2026-04-08 00:55:21 +0530

    Fix: re-added backend

--- backend/main.py
+++ backend/main.py
@@ -25,10 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://your-portfolio.vercel.app",
-                   ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
