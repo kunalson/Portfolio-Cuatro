72630717781982ad06a4f085b1ca884e5a2cc424
KunalSonz <kunalsonawane009@gmail.com>
2026-04-08 17:25:33 +0530

    Final Adjustment Before Actual Live

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,8 @@ print("Server starting...")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[ "http://localhost:3000",
+        "https://your-portfolio.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
